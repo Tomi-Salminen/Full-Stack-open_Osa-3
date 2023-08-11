b9e96b159dbedba7104281f843ae49f282417af7
Tomi Salminen <tomi.m.salminen@tuni.fi>
2023-08-11 14:02:26 +0300

    Tehtävä 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -52,6 +54,31 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+generateId = () => {
+    const id = Math.floor(Math.random() * (10 - 4) + 4)
+    return id
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'Content missing.'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    
+    persons = persons.concat(person)
+
+    res.json(person)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
