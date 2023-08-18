215f8be6ac74c8441730562bb0e60725ff35665f
Tomi Salminen <tomi.m.salminen@tuni.fi>
2023-08-18 10:51:48 +0300

    Tehtävä 3.9

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 var morgan = require('morgan')
 
@@ -7,6 +8,7 @@ morgan.token('body', function returnBody (req) {
 })
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
@@ -110,7 +112,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
